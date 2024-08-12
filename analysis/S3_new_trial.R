bcdc80d3ab22583280dc000494463281c118c057
Mathyn Vervaart <58030182+matverv@users.noreply.github.com>
2024-08-12 10:29:03 +0200

    Update S3_new_trial.R

--- analysis/S3_new_trial.R
+++ analysis/S3_new_trial.R
@@ -5,8 +5,8 @@
 #
 # This is supplementary material for the paper:
 #
-#  Calculating the Expected Net Benefit of Sampling for Survival Data: 
-#                  A Tutorial and Case Study. 2024.
+# Vervaart, M. (2024). Calculating the Expected Net Benefit of Sampling for Survival Data: 
+#   A Tutorial and Case Study. Medical Decision Making, in press.
 # 
 # Please cite the article when using this code.
 #
